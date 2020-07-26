13e5032eee0bcb1d5fa83034f0b84e75b0e79c55
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-26 02:53:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest = 100
- name_hash.each do |key, value|
+ name_hash.each do |value, key|
     if value > nil && value < smallest
      smallest = key
    end
